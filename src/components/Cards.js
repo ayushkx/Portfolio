e8941fc39237ab54b6af7c5141b9cdac2b699673
iaish-max <aishwarybhadauriya09@gmail.com>
2021-10-24 13:08:53 +0530

    first commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,26 +1,58 @@
-import React from 'react'
-import './project.css'
+import React from "react";
+import { useState } from "react";
+import "./project.css";
+import "./card.css";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
 
-export default function Cards(props) {
-    return (
-        <div>
-            <div>
-                    <p>
-                    <img src={`../assets/${props.img}.jpg`} style={{borderRadius: '50%'}} height="150px" width="150px" alt="project"/>
-                    </p>
-                    <h4 className="card-title">{props.name}</h4>
-                    <hr />
-                    <p className="card-text">{props.description}</p>
-                    <span className="mt-auto">
-                    <a className="github-icon" href={`${props.repolink}`} target="_blank" title="Repository" style={{margin: '10px 15px 10px 15px'}} >
-                        <img height="40" src="../assets/github.svg" alt="github"/>
-                    </a>
-                    <a className="github-icon" href={`${props.deployLink}`} target="_blank" title="Repository" style={{margin: '10px 15px 10px 15px'}} >
-                        <img height="32" src="../assets/link.svg" alt="link"/>
-                    </a>
-                    </span>
-                </div>                     
-
+const Cards = ({ name, description, repolink, deploylink }) => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div>
+      <div className="accordion-item projectCardDiv">
+        <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+          {!isActive && (
+            <div className="card-titleDiv">
+              <h2 className="card-title h2">{name}</h2>
+            </div>
+          )}
+          {isActive && (
+            <div className="card-titleDiv">
+              <h2 className="card-title active h2">{name}</h2>
+            </div>
+          )}
+          <div className="card-sign">
+            {isActive ? <p className="sign">-</p> : <p className="sign">+</p>}
+          </div>
         </div>
-    )
-}
+        {isActive && (
+          <div className="accordion-content">
+            <p className="card-text">{description}</p>
+            <span className="mt-auto">
+              <a
+                className="github-icon"
+                href={repolink}
+                target="_blank"
+                title="Repository"
+                style={{ margin: "10px 15px 10px 15px" }}
+              >
+                <AiOutlineGithub className="git-icon" />
+              </a>
+              <a
+                className="github-icon"
+                href={deploylink}
+                target="_blank"
+                title="Repository"
+                style={{ margin: "10px 15px 10px 15px" }}
+              >
+                <FiExternalLink class="link-icon" />
+              </a>
+            </span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import "./Navbar.css";
 
-
- export default function Navbar(props) {
+export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-    <div className="container-fluid">
-    <a className="navbar-brand" href = "/">{props.name}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href = "/">Home</a>
+    <div class="navDiv">
+      <ul>
+        <li class="navLi">
+          <a href="#home">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href = "/">{props.about} </a>
+        <li class="navLi">
+          <a href="#about">About</a>
+        </li>
+        <li class="navLi">
+          <a href="#project">Project</a>
+        </li>
+        <li class="navLi">
+          <a href="achievement">Achievement</a>
+        </li>
+        <li class="navLi">
+          <a href="language">Language</a>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
-  </div>
-</nav> 
-
-  )
+  );
 }
 
 Navbar.propTypes = {
-    name: PropTypes.string,
-    about : PropTypes.string
-  };
+  name: PropTypes.string,
+  about: PropTypes.string,
+};
 
 Navbar.defaultProps = {
-    name : "your title here" ,
-    about : "About here"
+  name: "your title here",
+  about: "About here",
 };
